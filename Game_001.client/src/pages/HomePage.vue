c6ae4342f372671f6fe5f4c9755403de52fbd55f
LemonadeGT1 <michael.p.patience@gmail.com>
2023-08-16 23:30:21 -0600

    init

---
+++ Game_001.client/src/pages/HomePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+    <div class="home-card p-5 bg-white rounded elevation-3">
+      <img src="../assets/img/Shady Dell-logos.jpeg" alt="Shady Dell Logo" class="rounded-circle">
+      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
+        Would you like to play a game?
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  display: grid;
+  height: 80vh;
+  place-content: center;
+  text-align: center;
+  user-select: none;
+
+  .home-card {
+    width: 50vw;
+
+    >img {
+      height: 200px;
+      max-width: 200px;
+      width: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
+}
+</style>
