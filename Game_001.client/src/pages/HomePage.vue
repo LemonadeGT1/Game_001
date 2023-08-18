cb655d6871ec120189a407a031663e58eb3137c3
LemonadeGT1 <michael.p.patience@gmail.com>
2023-08-17 22:18:25 -0600

    style updates

--- Game_001.client/src/pages/HomePage.vue
+++ Game_001.client/src/pages/HomePage.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="container pt-3 bg-white elevation-3 rounded mt-3">
     <div class="row p-2 align-items-center justify-content-center">
-      <div class="col-sm-3 col-12 text-center"><img src="../assets/img/Shady Dell-logos.jpeg" alt="Shady Dell Logo"
+      <div class="col-sm-3 col-12 text-center"><img src="../assets/img/Shady Dell-logos_black.png" alt="Shady Dell Logo"
           class="rounded-circle p-1 gameLogo"></div>
       <div class="col-sm-3 col-12 text-center"><img src="../assets/img/Shady Dell-logos.jpeg" alt="Shady Dell Logo"
           class="rounded-circle p-1 gameLogo"></div>
-      <div class="col-sm-3 col-12 text-center"><img src="../assets/img/Shady Dell-logos.jpeg" alt="Shady Dell Logo"
+      <div class="col-sm-3 col-12 text-center"><img src="../assets/img/MoonMinerLogo_001.png" alt="Shady Dell Logo"
           class="rounded-circle p-1 gameLogo"></div>
     </div>
     <div class="row p-2 px-3">
-      <h1 class="my-2 bg-dark text-white p-3 rounded text-center">
+      <h1 class="my-2 bg-success text-white p-3 rounded text-center">
         Would you like to play a game?
       </h1>
     </div>
@@ -26,6 +26,6 @@ export default {
 
 <style scoped lang="scss">
 .gameLogo {
-  width: 200px;
+  max-width: 200px;
 }
 </style>
